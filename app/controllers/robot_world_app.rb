2e6fd5864d072d7b514a774949a4165956cbfb24
VictoriaVasys <vvasys@gmail.com>
2017-03-15 22:22:56 -0600

    Robots create, index, edit, delete functions complete

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -2,9 +2,10 @@ require_relative '../models/robot.rb'
 
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
-    erb :layout
+    erb :dashboard
   end
   
   get '/robots' do
@@ -25,6 +26,21 @@ class RobotWorldApp < Sinatra::Base
   get '/robots/:id' do
     @robot = Robot.find(params[:id])
     erb :show
+  end
+  
+  get '/robots/:id/edit' do
+    @robot = Robot.find(params[:id])
+    erb :edit
+  end
+  
+  put '/robots/:id' do |id|
+    Robot.update(id.to_i, params[:robot])
+    redirect "/robots/#{id}"
+  end
+  
+  delete '/robots/:id' do |id|
+    Robot.destroy(id.to_i)
+    redirect '/robots'
   end
 end
 
--- config.ru
+++ config.ru
@@ -4,5 +4,6 @@ Bundler.require
 $LOAD_PATH.unshift(File.expand_path("app", __dir__))
 
 require 'controllers/robot_world_app'
+require 'pry-remote'
 
 run RobotWorldApp
